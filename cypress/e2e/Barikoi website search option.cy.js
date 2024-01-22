1e075aaeb5459176ceec216042f322e2a3fb0185
ahoteshanul <ahoteshanul@gmail.com>
2024-01-22 12:40:58 +0600

    clear repo

--- cypress/e2e/Barikoi website search option.cy.js
+++
@@ -1,7 +0,0 @@
-describe("BariKoi website search option", () => {
-  it('navigates', () => {
-    cy.visit("https://barikoi.com/search") 
-    cy.contains("Search").type('BariKoi')
-    cy.wait(5000)
-  })
-})
